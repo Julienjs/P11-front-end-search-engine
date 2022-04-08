fb0182f9b3f037a32daabe8799fb4ddbd820da61
Rozelier Julien <rozelierjjs@gmail.com>
2022-04-08 13:43:05 +0200

    version 2 de la barre de recherche principal

--- script/App.js
+++ script/App.js
@@ -1,4 +1,6 @@
 import SearchBar from './Search/SearchBar.js'
+import SearchBarV2 from './Search/SearchBarV2.js'
+
 import SearchTagFactory from './Search/SearchTagFactory.js';
 import RecipesDom from './Search/RecipesDom.js';
 
@@ -7,6 +9,7 @@ export default class App {
     constructor(recipes) {
         this.listTag = ["ingredients", "appliances", "ustensils"]   //tableau contenant les listes des tags
         this.listSearchTag = []   //tableau vides
+
         for (let list of this.listTag) {  //boucle sur le tableau contenant les listes des tags
             let searchTag = new SearchTagFactory(list, recipes) // envoi des parametres list et recipes a SearchTagFactory / joue la fonction
             this.listSearchTag.push(searchTag)  // ajout dans le tableau les differentes fonctions d'affichages des listes
@@ -15,14 +18,18 @@ export default class App {
             searchTag.displayListTag()  //permet de jouer la fonction pour ouvrir la liste au clic
             searchTag.hideTag()  //permet de jouer la fonction pour fermer la liste 
         }
-        let searchBar = new SearchBar(this.listSearchTag, recipes) // envoi en parametre le tableau contenant les list et les recettes / joue la fonction 
-        for (let list of this.listSearchTag) {
 
+        let searchBar = new SearchBar(this.listSearchTag, recipes) // envoi en parametre le tableau contenant les list et les recettes / joue la fonction 
+        let searchBarV2 = new SearchBarV2(this.listSearchTag, recipes) // envoi en parametre le tableau contenant les list et les recettes / joue la fonction 
+        for (let list of this.listSearchTag) {
+            searchBarV2.onTag(list)
             searchBar.onTag(list)
         }
 
         RecipesDom.showRecipes(recipes) //envoi des recettes en parametre / joue la fonction 
         searchBar.onSearch()  //joue la fonction qui permet de faire la recherche dans la barre principal
+        searchBarV2.onSearch()  //joue la fonction qui permet de faire la recherche dans la barre principal
+
     }
 
 
